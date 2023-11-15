3c52933506f19c3eeb22e3c96e49deb081e41a55
ankush kumar <ankush@gmail.com>
2023-11-15 10:26:33 +0530

    navbar layout completed successfully

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,17 +2,41 @@ import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Logo from "../assets/img/logo.svg";
+import NavbarIcon1 from "../assets/img/nav-icon1.svg";
+import NavbarIcon2 from "../assets/img/nav-icon2.svg";
+import NavbarIcon3 from "../assets/img/nav-icon3.svg";
 
 export const NavbarComponent = () => {
   const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <Navbar
       expand="lg"
-      className="bg-body-tertiary"
+      className={scrolled ? "scrolled" : ""}
       style={{ paddingTop: "0" }}
     >
       <Container>
-        <Navbar.Brand href="#home">Port</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggel-icon"></span>
         </Navbar.Toggle>
@@ -53,22 +77,23 @@ export const NavbarComponent = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {/* <img src={} alt=""/> */}
+                <img src={NavbarIcon1} alt="" />
               </a>
               <a
                 href="https://github.com/michaelwilliams"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {/* <img src={} alt=""/> */}
+                <img src={NavbarIcon2} alt="" />
               </a>
               <a
                 href="https://www.instagram.com/michaelwilliams/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {/* <img src={} alt=""/> */}
+                <img src={NavbarIcon3} alt="" />
               </a>
+              <button>Let's Connect</button>
             </div>
           </span>
         </Navbar.Collapse>
